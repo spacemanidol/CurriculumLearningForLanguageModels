e70fe36baed58b5cba0a5ff427aadbd7b90503db
spacemanidol <spacemanidol@gmail.com>
2020-05-07 16:16:01 -0700

    enwiki2 corpus curriculum

--- runcorpuscurriculum-wiki103.sh
+++ runcorpuscurriculum-wiki103.sh
@@ -23,3 +23,23 @@ python bin/restart.py --save_dir models/corpuscurricula/103 --vocab_file wikitex
 python bin/run_test.py --save_dir models/corpuscurricula/103 --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > corpus_curr_wiki103_validperplexity_epoch9
 python bin/restart.py --save_dir models/corpuscurricula/103 --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens> corpus_curr_wiki103_trainFull10.txt
 python bin/run_test.py --save_dir models/corpuscurricula/103 --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > corpus_curr_wiki103_validperplexity_epoch10
+python bin/train_elmo_1epoch.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens.5k --train_tokens wikitext2 baseline_corpus_curr_wiki103_train5k.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch1
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens.10k > baseline_corpus_curr_wiki103_train10k.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch2
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens.15k > baseline_corpus_curr_wiki103_train15k.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch3
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens.20k > baseline_corpus_curr_wiki103_train20k.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens >  baseline_corpus_curr_wiki103_validperplexity_epoch4
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens.25k >  baseline_corpus_curr_wiki103_train25k.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch5
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens.30k > baseline_corpus_curr_wiki103_train30k.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch6
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens > baseline_corpus_curr_wiki103_train-7.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch7
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens > baseline_corpus_curr_wiki103_train-8.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch8
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens > baseline_corpus_curr_wiki103_train-9.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch9
+python bin/restart.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt --train_prefix wikitext-103/wiki.train.tokens > baseline_corpus_curr_wiki103_train-10.txt
+python bin/run_test.py --save_dir models/standard/103/corpus_baseline --vocab_file wikitext-103/vocab.txt  --test_prefix wikitext-103/wiki.test.tokens > baseline_corpus_curr_wiki103_validperplexity_epoch10
--- runcorpuscurriculum-wiki2.sh
+++ runcorpuscurriculum-wiki2.sh
@@ -24,4 +24,24 @@ python bin/restart.py --save_dir models/corpuscurricula/2 --vocab_file wikitext-
 python bin/run_test.py --save_dir models/corpuscurricula/2 --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > corpus_curr_wiki2_validperplexity_epoch9
 python bin/restart.py --save_dir models/corpuscurricula/2 --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens > corpus_curr_wiki2_train-10.txt
 python bin/run_test.py --save_dir models/corpuscurricula/2 --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > corpus_curr_wiki2_validperplexity_epoch10
+python bin/train_elmo_1epoch.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens.5k --train_tokens wikitext2 baseline_corpus_curr_wiki2_train5k.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch1
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens.10k > baseline_corpus_curr_wiki2_train10k.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch2
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens.15k > baseline_corpus_curr_wiki2_train15k.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch3
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens.20k > baseline_corpus_curr_wiki2_train20k.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens >  baseline_corpus_curr_wiki2_validperplexity_epoch4
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens.25k >  baseline_corpus_curr_wiki2_train25k.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch5
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens.30k > baseline_corpus_curr_wiki2_train30k.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch6
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens > baseline_corpus_curr_wiki2_train-7.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch7
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens > baseline_corpus_curr_wiki2_train-8.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch8
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens > baseline_corpus_curr_wiki2_train-9.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch9
+python bin/restart.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt --train_prefix wikitext-2/wiki.train.tokens > baseline_corpus_curr_wiki2_train-10.txt
+python bin/run_test.py --save_dir models/standard/2/corpus_baseline --vocab_file wikitext-2/vocab.txt  --test_prefix wikitext-2/wiki.test.tokens > baseline_corpus_curr_wiki2_validperplexity_epoch10
 
