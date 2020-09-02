8b53668b45be37bf2c11861d109f3089e767d392
spacemanidol <dcampos3@illinois.edu>
2020-09-02 09:56:41 -0500

    more results being added to glue finetune

--- jiant_configs/jiant_config.sh
+++ jiant_configs/jiant_config.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # The base directory for model output.
-export JIANT_PROJECT_PREFIX=/home/spacemanidol/jiantexp
+export JIANT_PROJECT_PREFIX=/scratch/dcampos3/data
 
 # Base directory in which to look for raw data subdirectories. This
 # could be the glue_data directory created by download_glue_data.py.
-export JIANT_DATA_DIR=/home/spacemanidol/Data/jiantdata
+export JIANT_DATA_DIR=/scratch/dcampos3/Data
 export WORD_EMBS_FILE=None
